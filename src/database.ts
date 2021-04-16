ea2622a10b7cd9bb979270f8871f9ed2ec2a62d4
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-04-16 20:52:32 +0200

    🐛 Fix mongodb deprecation warning

--- src/database.ts
+++ src/database.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import config from './config'
 
 void mongoose.connect(config.dbUri, {
+  useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
