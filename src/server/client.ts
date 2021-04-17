a2dc8ee67be67765a0ac1c6fcb7eb881987d5ab3
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-04-17 17:40:24 +0200

    🔇 Remove console.log

--- src/server/client.ts
+++ src/server/client.ts
@@ -7,7 +7,6 @@ function applyClient(app: Express): void {
   app.use(express.static(resolve(config.client)))
 
   app.get('*', (_req, res) => {
-    console.log(join(resolve(config.client), 'index.html'))
     res.sendFile(join(resolve(config.client), 'index.html'))
   })
 }
