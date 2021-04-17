b1cd54a93b15d0574756d790dce86508b587545f
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-04-17 23:07:39 +0200

    ✨ Use mongoose-unique-validator

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose'
+import uniqueValidator from 'mongoose-unique-validator'
 
 import config from './config'
+
+mongoose.plugin(uniqueValidator)
 
 void mongoose.connect(config.dbUri, {
   useCreateIndex: true,
