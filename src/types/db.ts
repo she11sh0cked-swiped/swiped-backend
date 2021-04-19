a3c96e146d9afc097a8870fe05dc070fa493016b
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-04-19 11:53:31 +0200

    🏷️ Add resolve type

--- src/types/db.ts
+++ src/types/db.ts
@@ -1,3 +1,5 @@
 import { Document, ObjectId } from 'mongoose'
 
 export type TDocument<TSchema> = Document<ObjectId> & TSchema
+
+export type TResolve<TSchema> = Promise<{ record: TDocument<TSchema> }>
