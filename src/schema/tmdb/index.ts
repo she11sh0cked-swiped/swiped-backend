fd48038485bfb318c8f52af6b999766d6eb23bbc
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-04-21 21:24:30 +0200

    ♻️ Add a more generalized cachedCall method

--- src/schema/tmdb/index.ts
+++ src/schema/tmdb/index.ts
@@ -2,50 +2,14 @@ import { MovieDb } from 'moviedb-promise'
 
 import config from '~/config'
 import { QueryTmdb_MoviePopularArgs } from '~/types/api.generated'
-import cache from '~/utils/cache'
+import { cachedCall } from '~/utils/cache'
 import { schemaComposer } from '~/utils/graphql'
 import Schema from '~/utils/schema'
 
 import { popularMoviesRequestTC } from './api/requests'
 import { popularMoviesResponseTC } from './api/responses'
 
-type UnPromisify<T> = T extends Promise<infer U> ? U : T
-
-type KeysMatching<T extends unknown, V> = {
-  [K in keyof T]-?: T[K] extends V ? K : never
-}[keyof T]
-
-// eslint-disable-next-line @typescript-eslint/ban-types
-type TQueries = KeysMatching<MovieDb, Function>
-type TQueryParameters<TOperation extends TQueries> = Parameters<
-  MovieDb[TOperation]
->
-type TQueryResponse<TQuery extends TQueries> = UnPromisify<
-  ReturnType<MovieDb[TQuery]>
->
-
-const tmdbApi = new MovieDb(config.tmdbApiKey)
-
-async function cachedApiCall<TQuery extends TQueries>(
-  query: TQuery,
-  ...args: TQueryParameters<TQuery>
-): Promise<TQueryResponse<TQuery>> {
-  type TArgs = TQueryParameters<TQuery>
-  type TResponse = TQueryResponse<TQuery>
-  type TApiCall = (...args: TArgs) => Promise<TResponse>
-
-  const cacheKey = JSON.stringify({ args, query })
-
-  let response = cache.get(cacheKey)
-
-  if (response == null) {
-    const apiResponse = await (tmdbApi[query] as TApiCall)(...args)
-    response = apiResponse
-    cache.set(cacheKey, response)
-  }
-
-  return response as Promise<TResponse>
-}
+const api = new MovieDb(config.tmdbApiKey)
 
 const tmdb = new Schema('tmdb')
 
@@ -57,9 +21,7 @@ tmdb.addFields('queries', {
     args: popularMoviesRequestTC.getFields(),
     kind: 'query',
     name: 'tmdb_moviePopular',
-    async resolve({ args }) {
-      return await cachedApiCall('moviePopular', args)
-    },
+    resolve: ({ args }) => cachedCall(api.moviePopular.bind(api), args),
     type: popularMoviesResponseTC,
   }),
 })
--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -2,4 +2,26 @@ import NodeCache from 'node-cache'
 
 const cache = new NodeCache()
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type TFunction = (...args: any[]) => any
+
+type UnPromisify<T> = T extends Promise<infer U> ? U : T
+
+export async function cachedCall<
+  TMethod extends TFunction = TFunction,
+  TArgs extends Parameters<TMethod> = Parameters<TMethod>,
+  TResponse = UnPromisify<ReturnType<TMethod>>
+>(method: TMethod, ...args: TArgs): Promise<TResponse> {
+  const cacheKey = JSON.stringify({ args, method: method.name })
+
+  let response = cache.get<TResponse>(cacheKey)
+
+  if (response == null) {
+    response = await Promise.resolve<TResponse>(method(...args))
+    cache.set(cacheKey, response)
+  }
+
+  return response
+}
+
 export default cache
