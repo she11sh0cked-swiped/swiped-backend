7661b77e1993536dc972757b4ce74326c3a1f11b
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-04-21 17:17:56 +0200

    🔧 Add default for tmdbApiKey config option

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ const config = {
   dbUri: process.env.DB_URI ?? '',
   jwtSecret: process.env.JWT_SECRET ?? '',
   port: parseInt(process.env.PORT ?? ''),
+  tmdbApiKey: process.env.TMDB_API_KEY ?? '',
 }
 
 export default config
