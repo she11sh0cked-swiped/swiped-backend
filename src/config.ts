803c307f566b81e3e4ae1e32b7af455cf39f8a73
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-04-21 12:00:17 +0200

    ⚰️ Remove client env var

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,6 @@
 import 'dotenv-defaults/config'
 
 const config = {
-  client: process.env.CLIENT ?? '',
   dbUri: process.env.DB_URI ?? '',
   jwtSecret: process.env.JWT_SECRET ?? '',
   port: parseInt(process.env.PORT ?? ''),
